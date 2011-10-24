053a5427304da28a2cb0821547398510a5f08944
nancy_kiang <nkiang@giss.nasa.gov>
2011-10-24 17:40:12 -0400

    Same fixes to sapwood, ent_pfts_ENT.f sla, and sapwood C:N ratio, but in different working directory. Just to update this directory.

--- model/Ent/ent_prescribed_drv.f
+++ model/Ent/ent_prescribed_drv.f
@@ -309,7 +309,7 @@ ccc#endif
       logical,intent(in) :: do_read_from_files
 
       !-----Local------
-      integer :: i,j, jeq,p
+      integer :: i,j, jeq, p
       integer hemi(I0:I1,J0:J1)
       REAL*8 :: soil_C_total(N_CASA_LAYERS,I0:I1,J0:J1)
 
@@ -407,23 +407,20 @@ ccc        call stop_model("fix reading soil C for site", 255)
 #endif
       !print*,'vegdata(:,I1,J1)',vegdata(:,I1,J1)
       !print*,'hdata',hdata(:,I1,J1)
-      !print*,'nmdata',nmdata
+      !print*,'nmdata',nmdata(:,I1,J1)
       !print*,'dbhdata',dbhdata(:,I1,J1)
       !print*,'popdata',popdata(:,I1,J1)
       !print*,'craddata',craddata(:,I1,J1)
-      !print*,'laidata',laidata(:,I1,J1)
       !do p=1,N_PFT
       !     print*,'cpooldata pft=',p,
-      ! &     cpooldata(p+COVEROFFSET,:,I1,J1)
-      !enddo 
-      !do p=1,N_PFT
-      !   print *,'sla pft=',p,pfpar(p)%sla
-      !enddo
-      !do p=1,N_PFT
-      !   print *,'lrage pft=',p,pfpar(p)%lrage
-      !enddo
-      !print *,'vhght',vhght(:)
-      !print *,'alamax',alamax(:)
+      !&     cpooldata(p+COVEROFFSET,:,I1,J1)
+      ! enddo 
+!      print*,'cpooldata(GRASSC32+COVEROFFSET,:,I1,J1)',
+!     &     cpooldata(GRASSC3+COVEROFFSET,:,I1,J1)
+!    &     cpooldata(SHRUB+COVEROFFSET,:,I1,J1)
+!      print*,'cpooldata(SAVANNA+COVEROFFSET,:,I1,J1)',
+!     &     cpooldata(SAVANNA+COVEROFFSET,:,I1,J1)
+      !print*,'soil_color',soil_color
 
       end subroutine prescr_vegdata
 
