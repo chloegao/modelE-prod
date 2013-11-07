01cc68a6cd890cc7b924ce40cddc0bce9309426e
Audrey Wolf <awolf@giss.nasa.gov>
2013-11-07 15:27:16 -0500

    Updated SCM rundeck in templates  and changed SCM_DIAG  for separate cloud water/ice variables

--- templates/SCM.R
+++ templates/SCM.R
@@ -152,10 +152,10 @@ TAero_BCB=/path/to/user/directory/extractions/BCB_Koch2008_kg_m2_144x90x20_1890-
 ! modes of operation may also prescribe ocean surface conditions
 ! via mechanisms other than these files. SICE and ZSIFAC can be
 ! omitted if the simulation location is free of sea ice.
-OSST=/path/to/user/directory/extractions/OST_144x90.1996-2005avg.HadISST1.1.nc  ! SST
+!OSST=/path/to/user/directory/extractions/OST_144x90.1996-2005avg.HadISST1.1.nc  ! SST
 ! rsi var. in SICE is sea ice fraction, ZSIFAC var. dm is used to get ice thickness
-SICE=/path/to/user/directory/extractions/SICE_144x90.1996-2005avg.HadISST1.1.nc
-ZSIFAC=/path/to/user/directory/extractions/SICE_144x90.1996-2005avg.HadISST1.1.nc
+!SICE=/path/to/user/directory/extractions/SICE_144x90.1996-2005avg.HadISST1.1.nc
+!ZSIFAC=/path/to/user/directory/extractions/SICE_144x90.1996-2005avg.HadISST1.1.nc
 !OCNML=XXX ! see comment above regarding the OCNML source code.
 
 ! These land-surface files are only needed if the TOPO file for
@@ -290,7 +290,7 @@ variable_lk=1
 &&END_PARAMETERS
 
  &INPUTZ
- YEARI=2000,MONTHI=1,DATEI=4,HOURI=0, ! pick IYEAR1=YEARI (default) or < YEARI
- YEARE=2000,MONTHE=2,DATEE=1,HOURE=0,     KDIAG=12*0,9,
- ISTART=2,IRANDI=0, YEARE=2000,MONTHE=2,DATEE=1,HOURE=0,
+ YEARI=2005,MONTHI=1,DATEI=1,HOURI=0, ! pick IYEAR1=YEARI (default) or < YEARI
+ YEARE=2005,MONTHE=1,DATEE=31,HOURE=23,     KDIAG=12*0,9,
+ ISTART=2,IRANDI=0, YEARE=2005,MONTHE=1,DATEE=31,HOURE=23,
 /
