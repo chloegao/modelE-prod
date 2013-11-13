593165dc406e4b5ecb50bb32a8d4ee36aa895e58
Tom <Thomas.L.Clune@nasa.gov>
2013-11-13 11:29:36 -0500

    Correction to pfunit usage - better hiding of dependency for non PFUNIT users.

--- model/shared/PlanetaryCalendar.F90
+++ model/shared/PlanetaryCalendar.F90
@@ -8,9 +8,6 @@ module PlanetaryCalendar_mod
   use KindParameters_mod, only: WP => DP
   use TimeInterval_mod
   use Rational_mod
-#ifdef USE_PFUNIT
-  use pFUnit_mod, only: throw
-#endif
   implicit none
   private
 
@@ -132,7 +129,7 @@ contains
     call calendar%setDaysPerYear(daysPerYear)
 
     if (size(monthLongitudes) /= MONTHS_PER_YEAR) then
-       call throw('PlanetaryCalendar assumes 12 month years.')
+       call throwException('PlanetaryCalendar assumes 12 month years.')
        return
     end if
 
