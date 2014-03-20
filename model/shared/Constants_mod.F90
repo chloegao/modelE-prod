c7d47b8d2c1975b5b6d054c4eb41b41aab4c3aab
Carlos Cruz <carlos.a.cruz@nasa.gov>
2014-03-20 15:41:23 -0400

    Adding the variable pO2 from CONST.f (in AR5_v2_branch). See:
        git cherry-pick 7fe33c68c3a38e2832518512644318c17f6e74af

--- model/shared/Constants_mod.F90
+++ model/shared/Constants_mod.F90
@@ -91,6 +91,9 @@ module constant
 !@param byshi 1/shi
   real*8,parameter :: byshi = 1d0/shi
 
+!@param fraction of O2 in the atmosphere (0-1)
+      real*8,parameter :: pO2 = 0.209476d0
+
   !**** RGAS = R/M_A = 1000* 8.314510 J/mol K /28.9655 g/mol
   !**** For values of CO2 much larger than present day (> 4x conc)
   !**** the molar mass of dry air M_A could change.
