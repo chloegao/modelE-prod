288329033de0bd6347168e5ebeb1a895b1aa78db
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2015-04-01 14:32:31 -0400

    Increasing MAX_CPARAMS a bit

--- model/shared/Dictionary_mod.F90
+++ model/shared/Dictionary_mod.F90
@@ -163,7 +163,7 @@ module Dictionary_mod
   integer, parameter :: MAX_PARAMS =700
   integer, parameter :: MAX_RPARAMS =430
   integer, parameter :: MAX_IPARAMS = 1000 
-  integer, parameter :: MAX_CPARAMS = 500
+  integer, parameter :: MAX_CPARAMS = 520
   integer, parameter :: MAX_NAME_LEN = 32
   integer, parameter :: MAX_CHAR_LEN = 128
 
