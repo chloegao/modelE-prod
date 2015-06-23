6842527d069f97161308994ec9dcd54f3806125d
Igor Aleinov <ialeinov@giss.nasa.gov>
2015-06-23 01:45:13 -0400

    Reverting changes introduced by 81b3fda2312213fec383be5d6cf038672c731c1a commit. This commit is reverted because it introduces significant structural changes which were not properly discussed with core development team and which don't seem to belong to the master branch at its current stage.
    Sorry, had to undo all changes since they were not structured
    as separate commits. The only change that was left intact was the
    one related to stop_model.F90, since it was already discussed
    and didn't raise any objections.
    
    The changes from 81b3fda2312213fec383be5d6cf038672c731c1a
    commit will be pushed as a separate branch "Bob_cmake"
    so that no work is lost.
    
    Revert "CMake build and some Python upgrades"
    
    This reverts commit 81b3fda2312213fec383be5d6cf038672c731c1a.
    
    Conflicts:
    
    	cmake/setCompilerFlags.cmake
    	model/shared/libmodele_refaddr.c
    	model/shared/modele_python.F90
    	model/shared/stop_model.F90

--- model/profiler/profiler_SOURCES.cmake
+++
@@ -1,8 +0,0 @@
-set(profiler_SOURCES
-   ${CMAKE_CURRENT_SOURCE_DIR}/profiler/ProfileReport_mod.F90
-   ${CMAKE_CURRENT_SOURCE_DIR}/profiler/ReportColumn_mod.F90
-   ${CMAKE_CURRENT_SOURCE_DIR}/profiler/TimeFormatUtilities_mod.F90
-   ${CMAKE_CURRENT_SOURCE_DIR}/profiler/TimerList_mod.F90
-   ${CMAKE_CURRENT_SOURCE_DIR}/profiler/Timer_mod.F90
-   ${CMAKE_CURRENT_SOURCE_DIR}/profiler/TimerPackage_mod.F90
-)
