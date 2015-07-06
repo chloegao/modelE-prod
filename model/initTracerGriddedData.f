910400be2012eb4e2ee1ae098402e43fbf9e97c4
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2015-07-06 10:05:15 -0400

    Eliminating an unused variable

--- model/initTracerGriddedData.f
+++ model/initTracerGriddedData.f
@@ -56,7 +56,6 @@
      &     CFCICIN,CFCIC,CFCICINL,CFCICL,PIratio_N2O,PIratio_CFC,
      &     use_rad_n2o,use_rad_cfc,cfc_rad95,PltOx,Tpsc_offset_N,
      &     Tpsc_offset_S
-      use photolysis, only: rad_FL
 #ifdef INTERACTIVE_WETLANDS_CH4
       USE TRACER_SOURCES, only:int_wet_dist,topo_lim,sat_lim,gw_ulim,
      &gw_llim,sw_lim,exclude_us_eu,nn_or_zon,ice_age,nday_ch4,max_days,
