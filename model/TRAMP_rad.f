feiliuesmf/modelE

commit 7b3e1851ab8190baeba88a8cfe749f64b58466e2

Enabling E4TampF40 rundeck.
This commit is for a variety of minor fixes to restore the ability for
AMP tracers to work for the development branch of the model, as
determined by the E4TampF40 rundeck.

1) Adding/moving USE statements to make entities accessible in (presumably) new locations.

2) Added function aerosolKind() that pulls out appropriate substring
from tracer name and replaced the logic which uses the substring with
a select case.  (Necessary because trname() is now a function and
expressions like trname(n)(6:8) no longer are permitted.)

3) Added empty module to work around intel internal compiler error
that we thought was eliminated by the 13.0 release, but apparently
not.

model/TRAMP_rad.f
@@ -2,6 +2,16 @@
 
 c -----------------------------------------------------------------
 
+      module AMP_Utilities_mod
+
+      contains
+
+      character*2 function aerosolkind(tracerName) 
+      character(len=*), intent(in) :: tracerName
+      aerosolkind = tracerName(7:8)
+      end function aerosolkind
+
+      end module AMP_Utilities_mod
 
       SUBROUTINE SETAMP(EXT,SCT,GCB,TAB)
 !@sum Calculation of extinction, asymmetry and scattering for AMP Aerosols
@@ -321,12 +331,14 @@ c -----------------------------------------------------------------
 
       USE AMP_AEROSOL, only: DIAM, Reff_LEV, NUMB_LEV, RindexAMP,NUMB_SS,
      +                       dry_Vf_LEV,MIX_OC,MIX_SU,MIX_AQ,AMP_dens, AMP_RAD_KEY
-      USE TRACER_COM,  only: TRM, ntmAMPi,ntmAMPe, AMP_AERO_MAP,AMP_NUMB_MAP,AMP_MODES_MAP,trname
+      USE TRACER_COM,  only: TRM, ntmAMPi,ntmAMPe, AMP_AERO_MAP,AMP_NUMB_MAP,AMP_MODES_MAP
       USE AERO_CONFIG, only: NMODES
       USE AERO_SETUP,  only: SIG0, CONV_DPAM_TO_DGN   !(nmodes * npoints) lognormal parameters for each mode
       USE GEOM,        only: BYDXYP ! inverse area of gridbox [m-2]
 
       USE AERO_ACTV, only: DENS_SULF, DENS_DUST,DENS_SEAS, DENS_BCAR, DENS_OCAR
+      use OldTracer_mod, only: trname
+      use AMP_utilities_mod, only: aerosolkind
       IMPLICIT NONE
 
       ! Arguments: 
@@ -393,11 +405,18 @@ cBond + Berstroem, all wavelength
            if(trname(n) .eq.'M_NO3') NO3 =trm(i,j,l,n)
            if(trname(n) .eq.'M_H2O') H2O =trm(i,j,l,n)
           if(AMP_NUMB_MAP(nAMP).eq. 0) then  ! Volume fraction
-           if(trname(n)(6:8).eq.'_SU') VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_SULF
-           if(trname(n)(6:8).eq.'_BC') VMass(AMP_MODES_MAP(nAMP),2) =trm(i,j,l,n)/DENS_BCAR
-           if(trname(n)(6:8).eq.'_OC') VMass(AMP_MODES_MAP(nAMP),3) =trm(i,j,l,n)/DENS_OCAR
-           if(trname(n)(6:8).eq.'_DU') VMass(AMP_MODES_MAP(nAMP),4) =trm(i,j,l,n)/DENS_DUST
-           if(trname(n)(6:8).eq.'_SS') VMass(AMP_MODES_MAP(nAMP),5) =trm(i,j,l,n)/DENS_SEAS
+             select case (aerosolKind(trname(n)))
+             case ('SU')
+                VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_SULF
+             case ('BC')
+                VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_BCAR
+             case ('OC')
+                VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_OCAR
+             case ('DU')
+                VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_DUST
+             case ('SS')
+                VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_SEAS
+             end select
           else                           ! Number
 !          [ - ]                        [#/gb]         [m-2]      
            NUMB_LEV(l,AMP_NUMB_MAP(nAMP)) =trm(i,j,l,n) * bydxyp(j)

model/PBL_DRV.f
@@ -35,8 +35,7 @@
 #endif
 #endif
 #ifdef TRACERS_AMP
-      use TRACER_COM, only:
-     & ,AMP_MODES_MAP,AMP_NUMB_MAP,ntmAMPi,ntmAMPe
+      use TRACER_COM, only: AMP_MODES_MAP,AMP_NUMB_MAP,ntmAMPi,ntmAMPe
       USE AMP_AEROSOL, only : DIAM, AMP_dens,AMP_TR_MM
       USE AERO_SETUP,  only : CONV_DPAM_TO_DGN
 #endif

model/RAD_DRV.f
@@ -104,6 +104,11 @@ C****
 #endif
 #ifdef TRACERS_AMP
       USE AERO_CONFIG, only: nmodes
+      USE TRACER_COM, only:  
+     *     n_N_AKK_1 ,n_N_ACC_1 ,n_N_DD1_1 ,n_N_DS1_1 ,n_N_DD2_1,
+     *     n_N_DS2_1, n_N_SSA_1, n_N_SSC_1, n_N_OCC_1, n_N_BC1_1,
+     *     n_N_BC2_1 ,n_N_BC3_1,
+     *     n_N_DBC_1, n_N_BOC_1, n_N_BCS_1, n_N_MXX_1
 #endif
 #ifdef TRACERS_TOMAS
       USE TOMAS_AEROSOL, only: icomp

model/TRACERS_AEROSOLS_Koch_e4.f
@@ -725,6 +725,9 @@ C TOMAS don't allow offline SS!
       use TRACER_COM, only: n_AECOB, n_AECIL, n_AOCOB, n_AOCIL
       use TRACER_COM, only: n_AECIL, n_H2SO4, nbins
 #endif
+#ifdef TRACERS_AMP
+      use TRACER_COM, only: n_H2SO4
+#endif
       USE TRDIAG_COM, only : 
 #if (defined TRACERS_AEROSOLS_Koch) || (defined TRACERS_AMP) ||\
     (defined TRACERS_TOMAS)

model/TRACERS_DRV.f
@@ -9019,6 +9019,10 @@ c latlon grid
       use TRACER_COM, only: IDTECOB, IDTOCOB, IDTSO4, n_H2SO4, n_SOAGAS
       use TRACER_COM, only: nChemistry, n_AOCOB, n_AECIL, ntm_tomas
 #endif
+#ifdef TRACERS_AMP
+      use TRACER_COM, only: n_H2SO4
+      use TRACER_COM, only: ntmAMPi, ntmAMPe
+#endif
       USE CONSTANT, only : mair, avog
       USE FLUXES, only: tr3Dsource
       use model_com, only: modelEclock

model/TRAMP_drv.f
@@ -1,4 +1,9 @@
 #include "rundeck_opts.h"
+      ! empty module that enables ifort 13.0.1
+      !TODO: remove this
+      module foo
+      end module foo
+
       MODULE AMP_AEROSOL
 !@sum Driver for Aerosol Microphysics
 !@auth Susanne Bauer

model/initTracerMetadata.f
@@ -265,11 +265,11 @@
       USE AERO_ACTV, only: DENS_SULF, DENS_DUST,
      &     DENS_SEAS, DENS_BCAR, DENS_OCAR
       USE AERO_CONFIG, only: nbins
-      USE AMP_AEROSOL, only: AMP_DIAG_FC, AMP_RAD_KEY
       USE AERO_COAG, only : SETUP_KIJ
       USE AERO_SETUP
       USE AERO_NPF, only: SETUP_NPFMASS
       USE AERO_DIAM, only: SETUP_DIAM
+      use Tracer_com, only: ntmAMPi, ntmAMPe, ntmAMP
 #endif
 #ifdef TRACERS_GASEXCH_ocean_CO2
       USE obio_forc, only : atmCO2
@@ -3437,6 +3437,10 @@ c     call set_HSTAR(n, tr_RKD(n)*convert_HSTAR)
       use tracers_dust,only : imDust,prefDustSources,fracClayPDFscheme
      &     ,fracSiltPDFscheme
 #endif
+#ifdef TRACERS_AMP
+      USE AMP_AEROSOL, only: AMP_DIAG_FC, AMP_RAD_KEY
+#endif
+
       USE TRACER_COM, only: ef_fact3d, no_emis_over_ice
       use Model_com, only: itime
       implicit none

chloegao/modelE-prod

commit 686b31517c00a8f28ee74d655a07eaa1dcbf3ca5

Drive-by fix of bug introduced in commit 7b3e1851ab8190baeba88a8cfe749f64b58466e2

model/TRAMP_rad.f
@@ -413,13 +413,13 @@ cBond + Berstroem, all wavelength
                case ('SU')
                   VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_SULF
                case ('BC')
-                  VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_BCAR
+                  VMass(AMP_MODES_MAP(nAMP),2) =trm(i,j,l,n)/DENS_BCAR
                case ('OC')
-                  VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_OCAR
+                  VMass(AMP_MODES_MAP(nAMP),3) =trm(i,j,l,n)/DENS_OCAR
                case ('DU')
-                  VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_DUST
+                  VMass(AMP_MODES_MAP(nAMP),4) =trm(i,j,l,n)/DENS_DUST
                case ('SS')
-                  VMass(AMP_MODES_MAP(nAMP),1) =trm(i,j,l,n)/DENS_SEAS
+                  VMass(AMP_MODES_MAP(nAMP),5) =trm(i,j,l,n)/DENS_SEAS
                end select
              endif
            else                           ! Number