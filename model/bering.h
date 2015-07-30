5e5e7ce61e3396c5a931fd4e335b20b3234b0611
Nick Tausnev <ntausnev@giss.nasa.gov>
2015-07-30 15:41:10 -0400

    Combined position Bering strait for one and two degree resolutions

--- model/bering.h
+++ model/bering.h
@@ -13,13 +13,16 @@ c
 c --- thus, the pairs [(ipacn,jpac),(iatln,jatl)],[(ipacs,jpac),(iatls,jatl)]
 c --- refer to identical grid cells in physical space.
 
+#ifdef HYCOM2deg
 c --- 2deg hycom
-c     integer,parameter :: ipacn=67,ipacs=68,jpac= 95
-c     integer,parameter :: iatln= 2,iatls= 1,jatl=156
-
+      integer,parameter :: ipacn=67,ipacs=68,jpac= 95
+      integer,parameter :: iatln= 2,iatls= 1,jatl=156
+#endif
+#ifdef HYCOM1deg
 c --- 1deg hycom refined (389x360) or unrefined (359x360)
       integer,parameter :: ipacn=137,ipacs=138,jpac=189
       integer,parameter :: iatln= 2,iatls= 1,jatl=312
+#endif
 
       logical,parameter :: beropn=.true.    !  true if bering strait open
 c-----------------------------------------------------------------------------
