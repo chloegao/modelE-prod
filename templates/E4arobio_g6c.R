86b673a1fb6c36cec5c699194214a0528d5bc50d
Doron Feldman <doron.feldman@nasa.gov>
2015-09-02 04:44:09 -0400

    Add missing rundeck parameter
    This parameter defines an otherwise-undefined variable

--- templates/E4arobio_g6c.R
+++ templates/E4arobio_g6c.R
@@ -218,6 +218,8 @@ nssw=48         ! obio needs that in order to always restart from hour 0
 !!! atmCO2=289.9      !uatm for preindustrial runs
 atmCO2=0.             !prognostic atmCO2
 to_volume_MixRat=1    ! for tracer printout
+!!!solFe=0.02            ! default iron solubility
+solFe=0.05            ! enhanced iron solubility
 
 &&END_PARAMETERS
 
