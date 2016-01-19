5768e3f9cdfceffb03a02ec66136c3d4f3ef55b7
Maxwell Kelley <kelley@giss.nasa.gov>
2016-01-19 17:00:41 -0500

    Added temporary option to fall back to the old O2 specification - purely for the purpose of maintaining results in short runs that are as close as possible to the AR5_v2_branch model. Will be removed after any remaining discrepancies have been flushed out via this technique.

--- model/RADIATION.f
+++ model/RADIATION.f
@@ -743,7 +743,11 @@ C-----------------------------------------------------------------------
       REAL*8, dimension(13) ::
 C     GAS NUMBER    1         2    3      4    5         6           7
 C                 H2O       CO2   O3     O2  NO2       N2O         CH4
+#ifdef V2_O2_MODE /* temporary option to exactly match v2_branch */
+     *   PPMV80=(/0d0, 337.90d0, 0d0,    21d4,0d0,  .3012d0,   1.5470d0
+#else
      *   PPMV80=(/0d0, 337.90d0, 0d0,pO2*1.d6,0d0,  .3012d0,   1.5470d0
+#endif
      *     ,.1666d-03,.3003d-03, 0d0,   .978D-04,  .0010D-10,  .0420d0/)
 C              CCL3F1    CCL2F2   N2     CFC-Y       CFC-Z         SO2
 C     GAS NUMBER    8         9   10        11          12          13
