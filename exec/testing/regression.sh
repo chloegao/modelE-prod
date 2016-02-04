690ee7dc93a84b7363a6063d7d30e1f069066905
Carlos Cruz <carlos.a.cruz@nasa.gov>
2016-02-04 10:05:36 -0500

    Added help screen and improved documentation.

--- exec/testing/regression.sh
+++ exec/testing/regression.sh
@@ -1,60 +1,73 @@
 #!/bin/bash
-#
-# Runs regression.py and performs some basic regression testing
-#
-# Usage: regression.sh [RUNSRC1 RUNSRC2 ... RUNSRCN -c] clean
-#
-# Where [...] are optional arguments representing rundeck template
-# names and one additional flag, -c, to perform compile-only verification.
-#
-# Examples:
-#
-#   From decks directory, without arguments:
-#
-#   1)   ../exec/testing/regression.sh
-#   
-#   will regression test nonProduction_E_AR5_C12 and will write all results
-#   in decks directory.
-#
-#   2)   ../exec/testing/regression.sh E4F40 E4TcadiF40 Earobio_g6c -c
-#
-#   will compile-only E4F40, E4TcadiF40 and Earobio_g6c and all work will 
-#   be done in the decks directory.
-#
-#   3)   ../exec/testing/regression.sh E4F40 E4TcadiF40 E4TctomasF40
-#
-#   will run restart-regression on E4F40, E4TcadiF40 and E4TctomasF40.
-#
-#   4) ../exec/testing/regression.sh clean
-#
-#   will remove ALL the temporary files and directories created by the
-#   script.
-#
-# Errors, if any, will be printed on STDOUT.
-#
-# Caveats:
-#
-# 1) User must preload working env modules and set MODELERC
-#    - Builds with compiler specified in MODELERC
-#    - builds with COMPILE_WITH_TRAPS=YES
-# 2) Runs interactively
-#    - OK for small rundecks (so, example (3) is not recommended)
-# 3) No separate scratch space
-#    -  Builds and runs proceed in the decks directory
-#       - Beware of quotas
-#    - Builds and runs proceed sequentially
-# 4) No baseline testing is done - just internal consistency
-#    - serial vs 4 pes
-#    - checkpoint/restart vs continuous
-# 5) Needs python version 2.7.x
-#
+
+usage () {
+read -r -d '' help  <<-"EOT"
+Perform some basic regression testing in a user's local repository.
+IMPORTANT: Execute from decks directory only.
+
+Usage: ../exec/testing/regression.sh [RUNSRC1 ... RUNSRCN -c --clean --help]
+
+Where [...] are optional arguments representing rundeck template names
+and additional flags:
+  -c : perform compile-only verification
+  --clean : clean all temporary files and directories created by this script
+  --help : print this screen
+
+Examples:
+
+  Run without arguments:
+
+  A)   ../exec/testing/regression.sh
+  
+  will regression test nonProduction_E_AR5_C12 and will write all results
+  in decks directory.
+
+  B)   ../exec/testing/regression.sh E4F40 E4TcadiF40 Earobio_g6c -c
+
+  will compile-only E4F40, E4TcadiF40 and Earobio_g6c and all work will 
+  be done in the decks directory.
+
+  C)   ../exec/testing/regression.sh E4F40 E4TcadiF40 E4TctomasF40
+
+  will run restart-regression on E4F40, E4TcadiF40 and E4TctomasF40.
+
+  D) ../exec/testing/regression.sh --clean
+
+  will remove ALL the temporary files and directories created by the
+  script.
+
+Errors, if any, will be printed on STDOUT.
+
+Caveats:
+
+A) User must preload working env modules and set MODELERC
+   - Builds with compiler specified in MODELERC
+   - Builds without optimization ("-O0 -g")
+B) Runs interactively
+   - OK for small rundecks and quick "sanity checks"
+C) No separate scratch space
+   - Builds and runs proceed in the decks directory
+   - Beware of quotas
+   - Builds and runs proceed sequentially
+D) No baseline testing is performed - just internal consistency
+   - serial vs 4 pes
+   - checkpoint/restart vs continuous
+E) Needs python version 2.7.x
+
+EOT
+   echo -e "$help"
+   exit 1
+}
+
 clean () {
    local dirs=()
    rm -rf *.mk *.R *.diff *.log *cfg* *_bin templ
    dirs=`find . -maxdepth 1  -type l -exec ls -d {} \;`
-   for d in "${dirs[@]}"; do
-      rm -rf $(readlink $d)
-   done
+   if [ ! -z "${dirs}" ]; then
+      for d in "${dirs[@]}"; do
+         rm -rf $(readlink -q ${d})
+      done
+   fi
    find . -type l -exec rm {} \;
    exit 1
 }
@@ -72,8 +85,11 @@ fi
 cnt=0
 verification=restartRun
 if [ "$#" -gt 0 ]; then
-   if [ "$1" == "clean" ]; then
+   if [ "$1" == "--clean" ]; then
       clean
+   fi
+   if [ "$1" == "--help" ]; then
+      usage
    fi
    userArgs=( "$@" )
    rundecks=()
