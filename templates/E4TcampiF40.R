31a74c6a59d0b65c9474fef93b2f6b84d0add11e
Greg Faluvegi <gregory.s.faluvegi@nasa.gov>
2016-03-22 20:01:45 -0400

    Alter CH4 intial conditions for these 2 decks which had slightly non-standard values for year ~2000 conditions: ch4_init_sh=1.733 --> 1.750 ch4_init_nh=1.814  --> 1.855 I actually plan to remove these from the templates in a future commit, but wanted to change these now for testing. Alters answer for these two decks, of course.

--- templates/E4TcampiF40.R
+++ templates/E4TcampiF40.R
@@ -155,8 +155,8 @@ madaer=3         ! 3: updated aerosols          ; 1: default sulfates/aerosols
 #include "dust_params_vmp_matrix"
 #include "chemistry_params"
 ! The following 3 lines OVERWRITE the include chemistry_params values!!
-ch4_init_sh=1.733      ! init cond/fixed conditions SH CH4 ppmv
-ch4_init_nh=1.814      ! init cond/fixed conditions NH CH4 ppmv
+ch4_init_sh=1.750      ! init cond/fixed conditions SH CH4 ppmv
+ch4_init_nh=1.855      ! init cond/fixed conditions NH CH4 ppmv
 PI_run        = 0
 
 DTsrc=1800.      ! cannot be changed after a run has been started
