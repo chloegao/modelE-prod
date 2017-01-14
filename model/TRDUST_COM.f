ae24663da39921226985dc8048508d660d70aa9e
jan perlwitz <jan.p.perlwitz@nasa.gov>
2017-01-14 16:57:32 -0500

    Make syntax of declaration for array subClayWeights compliant with gfortran.

--- model/TRDUST_COM.f
+++ model/TRDUST_COM.f
@@ -59,8 +59,8 @@
 !@var subClayWeights  weights for masses in the sub bins of the clay size
 !@+     class for each soil dust tracer
       real( kind=8 ), dimension( ntm_clay, nSubClays ) :: subClayWeights
-     &     = (/ ( ( 0.009d0, 0.081d0, 0.234d0, 0.676d0 ), i = 1,ntm_clay
-     &     ) /)
+     &     = transpose( reshape ( (/ ( 0.009d0, 0.081d0, 0.234d0,
+     &     0.676d0, i = 1,ntm_clay ) /), (/ nSubClays, ntm_clay /) ) )
 
 c**** rundeck parameter to switch between different emission schemes
 c****
