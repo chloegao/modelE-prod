567cad3af47c21461931ea7232da9455cfc99ea6
Greg Faluvegi <gregory.s.faluvegi@nasa.gov>
2017-01-20 08:08:37 -0500

    increase kaij to allow E6 tracer decks to compile after esteemed leader's added diags yesterday.

--- model/DIAG_COM.f
+++ model/DIAG_COM.f
@@ -115,7 +115,7 @@ cmax      INTEGER, DIMENSION(IM,JM), public :: JREG
       REAL*8, ALLOCATABLE, DIMENSION(:,:,:), public :: ASJL,ASJL_loc
 
 !@param KAIJ number of AIJ diagnostics
-      INTEGER, PARAMETER, public :: KAIJ=520
+      INTEGER, PARAMETER, public :: KAIJ=535
 #ifdef ACCMIP_LIKE_DIAGS
      &                                   + 8
 #endif
