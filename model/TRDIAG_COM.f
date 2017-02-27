b4bdfc1190cb9c54520c8f3601a0f8d4cd780db4
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2017-02-27 10:45:35 -0500

    Increasing ktaijl

--- model/TRDIAG_COM.f
+++ model/TRDIAG_COM.f
@@ -204,7 +204,7 @@ C**** TAIJS  <<<< KTAIJS and IJTS_xx are Tracer-Dependent >>>>
 C**** TAIJLS 3D special tracer diagnostics
 
 !@param ktaijl number of TAIJLS tracer diagnostics;
-      INTEGER, PARAMETER :: ktaijl=104
+      INTEGER, PARAMETER :: ktaijl=105
 #ifdef ACCMIP_LIKE_DIAGS 
      &                            + 17
 #endif
