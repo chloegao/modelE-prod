5587c508c6d5e654ac3141e1bb410a9fc1bec94f
Maxwell Kelley <kelley@giss.nasa.gov>
2017-04-24 11:11:32 -0400

    Fixing what is likely a bug for the TOMAS case.   This may eventually be put back to production branches, but on this branch (tracersE3) it is being made so that an upcoming large-footprint commit can be shown not to change answers.

--- model/TRACERS.f
+++ model/TRACERS.f
@@ -84,7 +84,7 @@ C**** calculate fractional loss and update tracer mass
           if(trm(i,j,l,n).gt.0.)then
           fred(i)=max(0.d0,1.+min(0.d0,dtrm(i,j,l))/(trm(i,j,l,n)+eps))
           else
-             fred(i)=100.  !It won't be used anyway (fred<1 to be used)
+             fred(i)=1.  !It won't be used anyway (fred<1 to be used)
           endif
 #else
           fred(i) = max(0.d0,
