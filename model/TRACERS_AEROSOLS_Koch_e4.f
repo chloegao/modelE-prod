7b238fae6679f470da5d54eb5e06ddcd81fd93b2
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2017-06-26 19:17:23 -0400

    Fixing two MATRIX bugs:
    1) An index was incorrectly set, and H2SO4 changes were not applied. This was introduced in tracersE3, probably by me, so apologies for that. If you were using tracersE3 MATRIX for production, please update your code.
    
    2) The gas-phase conversion of SO2 to H2SO4 was not taking into account the MW change. This increases H2SO4 from gas-phase chemistry by 50%. Impact to be explored. The change will probably get ported to master.

--- model/TRACERS_AEROSOLS_Koch_e4.f
+++ model/TRACERS_AEROSOLS_Koch_e4.f
@@ -875,7 +875,8 @@ c oxidation of SO2 to make SO4: SO2 + OH -> H2SO4
 #else
        tr3Dsource(l,nChemloss,n) = -trm_col(l,n)*(1.d0-d4)/dtsrc 
 #ifdef TRACERS_AMP
-       tr3Dsource(l,2,n_H2SO4)=trm_col(l,n)*(1.d0-d4)/dtsrc 
+       tr3Dsource(l,nOther,n_H2SO4)=trm_col(l,n)*(1.d0-d4)/dtsrc 
+     &      *tr_mm(n_H2SO4)/tr_mm(n) 
 #endif  /* TRACERS_AMP */
 #ifdef TRACERS_TOMAS
        H2SO4_chem(i,j,l)=trm_col(l,n)*(1.d0-d4)/dtsrc 
