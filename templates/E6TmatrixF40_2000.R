85987fa398c8a6a76ae9e3371bd6791814ce1b48
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2017-08-05 10:09:05 -0400

    Suffix _nosoa is no more needed in tracersE3

--- templates/E6TmatrixF40_2000.R
+++ templates/E6TmatrixF40_2000.R
@@ -98,7 +98,7 @@ NAMERVR=RD_Fb.names.txt  ! named river outlets
 #include "land144x90_input_files"
 #include "rad_input_files"
 #include "rad_144x90_input_files_CMIP6"
-#include "chemistry_input_files_nosoa"
+#include "chemistry_input_files"
 #include "chemistry_144x90_input_files"
 #include "dust_tracer_144x90_input_files"
 #include "dry_depos_144x90_input_files"
