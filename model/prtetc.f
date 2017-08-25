4994d2395435a69a39f8ff19a49a799197f7efd2
Nick Tausnev <ntausnev@giss.nasa.gov>
2017-08-25 09:15:21 -0400

    Replaced lp/logical printer to equal standard output. Let print arrays on distributed grids

--- model/prtetc.f
+++ model/prtetc.f
@@ -4,7 +4,6 @@ c
 c --- Delete 'array' elements outside 'mask'. Then
 c --- break 'array' into sections, each 'nchar' characters wide, for printing.
 c
-      USE HYCOM_SCALARS, only : lp
       USE HYCOM_DIM, only: jdm
       implicit none
 c
@@ -29,10 +28,10 @@ c
       j1=ncols*(n-1)+1
       j2=min0(ncols*n,jj)
       if (j1.gt.j2) go to 1
-      write (lp,'(/'' Sec.'',i2,'' (cols'',i4,'' -'',i4,'') -- '',a)')
+      write (*,'(/'' Sec.'',i2,'' (cols'',i4,'' -'',i4,'') -- '',a)')
      .   n,j1,j2,title
 ccc      if (j2.lt.j1+5) then
-ccc      write (lp,'('' (Not printed. Too few columns. Save paper.)'')')
+ccc      write (*,'('' (Not printed. Too few columns. Save paper.)'')')
 ccc      go to 1
 ccc      end if
       do 2 i=1,ii
@@ -40,15 +39,43 @@ ccc      end if
  3    work(i,j)=cvmgz(0.,array(i,j),mask(i,j))
       do 4 j=j1,j2
  4    work(i,j)=cvmgz(0.,(work(i,j)-offset)*scale,mask(i,j))
-ccc 2    write (lp,'(32i4)') (int(work(i,j)),j=j1,j2)
- 2    write (lp,'(32i4)') i,(int(work(i,j)),j=j1,j2)
+ccc 2    write (*,'(32i4)') (int(work(i,j)),j=j1,j2)
+ 2    write (*,'(32i4)') i,(int(work(i,j)),j=j1,j2)
  1    continue
       return
       end
 c
+c
+      subroutine prifld(array,idm,ii,jj,what)
+
+! --- break  i n t e g e r  'array' into sections, each 'nchar' characters
+! --- wide, for printing.
+
+      implicit none
+      integer,intent(IN)   :: idm,ii,jj,array(idm,jj)
+      character,intent(IN) :: what*(*)
+      integer ncols,i,j,j1,j2,n
+      integer,parameter :: nchar=76
+!     integer,parameter :: nchar=132
+
+      ncols=nchar/4				!  each number gets 4 spaces
+!     ncols=nchar/9				!  each number gets 9 spaces
+      do 1 n=1,(jj-1)/ncols+1
+      j1=ncols*(n-1)+1
+      j2=min(ncols*n,jj)
+      if (j1.gt.j2) go to 1
+      write (*,'(/" Sec.",i2," (cols ",i3,"-",i3,") -- ",a)')
+     .   n,j1,j2,what
+      do 2 i=1,ii
+      write (*,'(36i4)') i,(array(i,j),j=j1,j2) 		!  4 spaces
+ 2    continue
+ 1    continue
+      return
+      end subroutine prifld
+c
+c
       subroutine linout(value,char,index)
 c
-      USE HYCOM_SCALARS, only : lp
       implicit none
       real value
       integer index
@@ -70,9 +97,10 @@ c
         n=int(mod(value,float(length)))
         line(n)=char
       end if
-      if (index.gt.0) write (lp,'(''=+='',80a1)') (line(l),l=1,length)
+      if (index.gt.0) write (*,'(''=+='',80a1)') (line(l),l=1,length)
       return
       end
+c
 c
       subroutine pipe_init(master)
 c
@@ -129,14 +157,14 @@ c
 c
       if (slave) then
       write (lpunit,'(2a)') 'writing for comparison: ',what
-      write (lp    ,'(2a)') 'writing for comparison: ',what
+      write (*     ,'(2a)') 'writing for comparison: ',what
       write (iunit) what,field
 c
       else                                !  slave = .false.
 c
       read (iunit) which,field1
       write (lpunit,'(2a)') 'reading for comparison: ',which
-      write (lp    ,'(2a)') 'reading for comparison: ',which
+      write (*     ,'(2a)') 'reading for comparison: ',which
       if (what.ne.which) then
         write (lpunit,'(4a)') 'out of sync -- trying to compare ',what,
      .     '  to  ',which
@@ -159,6 +187,7 @@ c
       end if
       return
       end
+c
 c
       subroutine compare_r4(field,mask,what)
       USE HYCOM_DIM_GLOB, only : idm,jdm
@@ -178,14 +207,14 @@ c
 c
       if (slave) then
       write (lpunit,'(2a)') 'writing r4 for comparison: ',what
-      write (lp    ,'(2a)') 'writing r4 for comparison: ',what
+      write (*     ,'(2a)') 'writing r4 for comparison: ',what
       write (iunit) what,field
 c
       else                                !  slave = .false.
 c
       read (iunit) which,field1
       write (lpunit,'(2a)') 'reading r4 for comparison: ',which
-      write (lp    ,'(2a)') 'reading r4 for comparison: ',which
+      write (*     ,'(2a)') 'reading r4 for comparison: ',which
       if (what.ne.which) then
         write (lpunit,'(4a)') 'out of sync -- trying to compare ',what,
      .     '  to  ',which
@@ -208,6 +237,7 @@ c
       end if
       return
       end
+c
 c
       subroutine comparall(m,n,mm,nn,info)
 c
@@ -248,25 +278,24 @@ c
       return
       end
 c
+c
       subroutine findmx(mask,array,idm,ii,jj,name)
 c
 c --- find maximum and minimum in 'array'. only check points where mask > 0
 c
       USE HYCOM_DIM_GLOB, only : jchunk
-      USE HYCOM_SCALARS, only : lp
+      USE HYCOM_SCALARS, only : huge
       implicit none
       integer jmax
       parameter (jmax=2000)
       integer i,j,idm,ii,jj,mask(idm,jj),ipos,jpos,ineg,jneg,
      .        jpoj(jmax),ipoj(jmax),jnej(jmax),inej(jmax)
-      real array(idm,jj),difpos,difneg,difpoj(jmax),difnej(jmax),huge
+      real array(idm,jj),difpos,difneg,difpoj(jmax),difnej(jmax)
       character name*(*)
-      data huge/1.e33/
 c
-!!! hack : not defined ?
-      ipos = 0 ; jpos = 0
-      ineg = 0 ;jneg = 0
-
+      ipos = -1 ; jpos = -1
+      ineg = -1 ; jneg = -1
+c
       if (jj.gt.jmax) stop '(error subr.findmx -- jmax < jj)'
 c
       do j=1,jj
@@ -315,11 +344,12 @@ c
         end if
       end do
 c
-      write (lp,'(2a,1p,2(e11.2,2i5))') name,'  min,max =',
+      write (*,'(2a,1p,2(e11.2,2i5))') name,'  min,max =',
      .    difneg,ineg,jneg,difpos,ipos,jpos
 c
       return
       end
+c
 c
       subroutine stencl(iz,jz,k,mn)
 c
@@ -327,7 +357,7 @@ c --- write 5 x 5 point cluster of grid point values centered on (iz,jz)
 c --- input parameters: k = layer index; mn = time slot index, i.e., mm or nn
 c
       USE HYCOM_DIM_GLOB, only : kk
-      USE HYCOM_SCALARS, only : lp,onem
+      USE HYCOM_SCALARS, only : onem
       USE HYCOM_ARRAYS_GLOB
       implicit none
       integer i,j,k
@@ -346,48 +376,48 @@ c105  format(i3,0p,5f7.1,5x,3p,5f7.2)                        !  cgs units
  106  format(i3,5f7.1,5x,2p,5f7.1)                        !  SI units
 c106  format(i3,-2p,5f7.1,5x,2p,5f7.1)                        !  cgs units
 c
-      write (lp,99) 'ice cover',' ice cover',(j,j=jz-2,jz+2)
+      write (*,99) 'ice cover',' ice cover',(j,j=jz-2,jz+2)
      .   ,(j,j=jz-2,jz+2)
-      write (lp,106)
+      write (*,106)
      .  (i,(oice(i,j),j=jz-2,jz+2),
      .     (oice(i,j),j=jz-2,jz+2),i=iz-2,iz+2)
 c
-      write (lp,99) '  ubavg   ','   vbavg  ',(j,j=jz-2,jz+2)
+      write (*,99) '  ubavg   ','   vbavg  ',(j,j=jz-2,jz+2)
      .   ,(j,j=jz-2,jz+2)
-      write (lp,103)
+      write (*,103)
      .  (i,(ubavg(i,j,1),j=jz-2,jz+2),
      .     (vbavg(i,j,1),j=jz-2,jz+2),i=iz-2,iz+2)
 c
       do 1 ks=1,kk                                !  print out all layers
 ccc   do 1 ks=max(1,k-1),min(kk,k+1)                !  print out 3 adjacent layers
 c
-      write (lp,100) 'u at k=',ks,'v at k=',ks,(j,j=jz-2,jz+2)
+      write (*,100) 'u at k=',ks,'v at k=',ks,(j,j=jz-2,jz+2)
      .   ,(j,j=jz-2,jz+2)
-      write (lp,103)
+      write (*,103)
      .  (i,(u(i,j,ks+mn),j=jz-2,jz+2),
      .     (v(i,j,ks+mn),j=jz-2,jz+2),i=iz-2,iz+2)
 c
-      write (lp,100) 'uflx k=',ks,'vflx k=',ks,(j,j=jz-2,jz+2)
+      write (*,100) 'uflx k=',ks,'vflx k=',ks,(j,j=jz-2,jz+2)
      .   ,(j,j=jz-2,jz+2)
-      write (lp,101)
+      write (*,101)
      .  (i,(uflx(i,j,ks),j=jz-2,jz+2),
      .     (vflx(i,j,ks),j=jz-2,jz+2),i=iz-2,iz+2)
 c
-      write (lp,100) 'temp k=',ks,'saln k=',ks,(j,j=jz-2,jz+2)
+      write (*,100) 'temp k=',ks,'saln k=',ks,(j,j=jz-2,jz+2)
      .   ,(j,j=jz-2,jz+2)
-      write (lp,104)
+      write (*,104)
      .  (i,(temp(i,j,ks+mn),j=jz-2,jz+2),
      .     (saln(i,j,ks+mn),j=jz-2,jz+2),i=iz-2,iz+2)
 c
-      write (lp,100) 'dp_o k=',ks,'dp_n k=',ks,(j,j=jz-2,jz+2)
+      write (*,100) 'dp_o k=',ks,'dp_n k=',ks,(j,j=jz-2,jz+2)
      .   ,(j,j=jz-2,jz+2)
-      write (lp,102)
+      write (*,102)
      .  (i,(dpold(i,j,ks)/onem,j=jz-2,jz+2),
      .     (dp(i,j,ks+mn)/onem,j=jz-2,jz+2),i=iz-2,iz+2)
 c
-      write (lp,100) 'pres k=',ks+1,'th3d k=',ks,(j,j=jz-2,jz+2)
+      write (*,100) 'pres k=',ks+1,'th3d k=',ks,(j,j=jz-2,jz+2)
      .   ,(j,j=jz-2,jz+2)
-      write (lp,105)
+      write (*,105)
      .  (i,(p(i,j,ks+1)/onem,j=jz-2,jz+2),
      .  (th3d(i,j,ks),j=jz-2,jz+2),i=iz-2,iz+2)
 c
@@ -396,13 +426,14 @@ ccc      if (1.gt.0) stop '(stencl)'                !  optional
       return
       end
 c
+c
       subroutine prt9x9(array,iz,jz,offset,scale,what)
 c
 c --- write 9 x 9 point cluster of 'array' values centered on (iz,jz).
 c --- the printed numbers actually represent (array(i,j) + offset) * scale
+c --- NOTE: matrix notation, i increases downward, j to the right
 c
       USE HYCOM_DIM_GLOB
-      USE HYCOM_SCALARS, only : lp
       implicit none
       integer i,j
 c
@@ -414,8 +445,8 @@ c
  100  format(a10,2x,9i7)
  101  format(i10,3x,9f7.1)
 c
-      write (lp,100) what,(jwrap(j),j=jz-4,jz+4)
-      write (lp,101) (i,(scale*(array(i,jwrap(j))+offset),
+      write (*,100) what,(jwrap(j),j=jz-4,jz+4)
+      write (*,101) (i,(scale*(array(i,jwrap(j))+offset),
      .   j=jz-4,jz+4),i=iz-4,iz+4)
 c
       return
@@ -424,52 +455,30 @@ c
 c
       subroutine pr_9x9(array,idm,jdm,iz,jz,offset,scale,what)
 c
-c --- (sames as prt9x9 except that array dimensions are passed as arguments)
-c
 c --- write 9 x 9 point cluster of 'array' values centered on (iz,jz).
 c --- the printed numbers actually represent (array(i,j) + offset) * scale
+c --- NOTE: matrix notation, i increases downward, j to the right
 c
-      USE HYCOM_SCALARS, only : lp
       implicit none
-      integer idm,jdm,i,j,iz,jz,jwrap
-      real array(idm,jdm),scale,offset
-      character what*12
+      integer  ,intent(IN) :: idm,jdm,iz,jz
+      real     ,intent(IN) :: array(idm,jdm),scale,offset
+      character,intent(IN) :: what*(*)
+      integer jwrap,i,j
+      character string*12
       jwrap(j)=mod(j-1+jdm,jdm)+1               !  for use in cyclic domain
 c
- 100  format(a12,9i7)
+ 100  format(/a12,9i7)
  101  format(i10,3x,9f7.1)
 c
-      write (lp,100) what,(jwrap(j),j=jz-4,jz+4)
-      write (lp,101) (i,(scale*(array(i,jwrap(j))+offset),
-     .   j=jz-4,jz+4),i=iz-4,iz+4)
+      string='            '
+      string(1:len_trim(what))=trim(what)
+      write (*,100) string,(jwrap(j),j=jz-4,jz+4)
+      write (*,101) (i,(scale*(array(i,jwrap(j))+offset),
+     .   j=jz-4,jz+4),i=max(1,iz-4),min(idm,iz+4))
 c
       return
       end
 c
-c
-      subroutine prt7x7(array,iz,jz,what)
-c
-c --- write 7 x 7 point cluster of 'array' values centered on (iz,jz).
-c
-      USE HYCOM_DIM_GLOB
-      USE HYCOM_SCALARS, only : lp
-      implicit none
-      integer i,j
-c
-      real array(idm,jdm),scale,offset
-      character what*12
-      integer iz,jz,jwrap
-      jwrap(j)=mod(j-1+jj,jj)+1		!  for use in cyclic domain
-c
- 100  format(a10,2x,7i9)
- 101  format(i10,3x,1p,7e9.1)
-c
-      write (lp,100) what,(jwrap(j),j=jz-3,jz+3)
-      write (lp,101) (i,(array(i,jwrap(j)),
-     .   j=jz-3,jz+3),i=iz-3,iz+3)
-c
-      return
-      end
 c
       subroutine psmo1(alist,pbot)
 c
@@ -527,34 +536,35 @@ c
       subroutine psmoo(alist)
 c
 c --- ragged boundary version of basic 1-2-1 smoothing routine
-c --- smoothed array overwrites input array -alist- at points where ip > 0
+c --- smoothed array overwrites input array -alist- at points where ogrid > 0
 c --- this routine is set up to smooth data carried at -p- points
 c
-c --- this version works for both cyclic-in-j and noncyclic domains
+      USE HYCOM_DIM,only : ogrid,isp,ifp,ilp,kk,idm,J_0,J_1,J_0H,J_1H
+      USE DOMAIN_DECOMP_1D, only : HALO_UPDATE,NORTH,SOUTH
+      use hycom_dim, only: ip
 c
-      USE HYCOM_DIM_GLOB
       implicit none
-c
-      real,intent(INOUT) :: alist(idm,jdm)
+      real,intent(INOUT) :: alist(idm,J_0H:J_1H)
       integer i,j,l,ia,ib,ja,jb
-      real blist(idm,jdm)
+      real blist(idm,J_0H:J_1H)
       real,parameter :: wgt=.25
 c
-      do 1 j=1,jj
+      CALL HALO_UPDATE(ogrid,alist, FROM=SOUTH+NORTH)
+      do 1 j=J_0,J_1
       do 1 l=1,isp(j)
       do 1 i=ifp(j,l),ilp(j,l)
-      ja=mod(j-2+jj,jj)+1
+      ja = PERIODIC_INDEX(j-1, jj)
+      jb = PERIODIC_INDEX(j+1, jj)
       if (ip(i,ja).eq.0) ja=j
-      jb=mod(j     ,jj)+1
       if (ip(i,jb).eq.0) jb=j
  1    blist(i,j)=(1.-wgt-wgt)*alist(i,j)+wgt*(alist(i,ja)+alist(i,jb))
 c
-      do 2 j=1,jj
+      do 2 j=J_0,J_1
       do 2 l=1,isp(j)
       do 2 i=ifp(j,l),ilp(j,l)
       ia=max( 1,i-1)
+      ib=min(idm,i+1)
       if (ip(ia,j).eq.0) ia=i
-      ib=min(ii,i+1)
       if (ip(ib,j).eq.0) ib=i
  2    alist(i,j)=(1.-wgt-wgt)*blist(i,j)+wgt*(blist(ia,j)+blist(ib,j))
       return
@@ -591,7 +601,7 @@ c
       do 2 i=ifu(j,l),ilu(j,l)
       ia=max( 1,i-1)
       if (iu(ia,j).eq.0) ia=i
-      ib=min(ii,i+1)
+      ib=min(idm,i+1)
       if (iu(ib,j).eq.0) ib=i
  2    alist(i,j)=(1.-wgt-wgt)*blist(i,j)+wgt*(blist(ia,j)+blist(ib,j))
       return
@@ -634,9 +644,11 @@ c
       return
       end
 c
+c
       subroutine psmooo(alist)
 c
-c --- ragged boundary version of basic 1-2-1 smoothing routine
+c --- ragged boundary version of  h e a v y  smoothing routine performing a
+c --- successsion of 1-2-1 and 1-0-1 smoothing operations in both directions.
 c --- alist is smoothed array, blist is work array
 c --- this routine is set up to smooth data carried at -p- points
 c
@@ -698,9 +710,9 @@ c
 c --- ragged boundary version of basic 1-2-1 smoothing routine
 c --- smoothed array overwrites input array -alist- at points where ip > 0
 c --- this routine is set up to smooth data carried at -p- points
-c 
+c
 c --- this version works for both cyclic-in-j and noncyclic domains
-c 
+c
       USE HYCOM_DIM_GLOB
       implicit none
       integer i,j,l,ia,ib,ja,jb
@@ -772,7 +784,7 @@ c
 c --- ragged boundary version of basic 1-2-1 smoothing routine
 c --- smoothed array overwrites input array -alist- at poins where iv > 0
 c --- this routine is set up to smooth data carried at -v- points
-c 
+c
 c --- this version works for both cyclic-in-j and noncyclic domains
 c
       USE HYCOM_DIM_GLOB
@@ -820,7 +832,6 @@ c
 c --- ii  may be smaller than  idim, the first (row) dimension of 'array'
 c --- in the calling program. thus, plotting of partial arrays is possible.
 c
-      USE HYCOM_SCALARS, only : lp
       implicit none
       integer lgth,idim,ii,jj,i,j
       parameter (lgth=1600)
@@ -830,7 +841,7 @@ c
       real array(idim,jj)
       data sqrt2/1.414/
 c
-      write (lp,'(a,3i6)') 'ZEBRA call with arguments',idim,ii,jj
+      write (*,'(a,3i6)') 'ZEBRA call with arguments',idim,ii,jj
       if (jj.gt.lgth) stop '(insuff. workspace in zebra: increase lgth)'
 c
       do 1 j=1,jj
@@ -871,7 +882,7 @@ c
  2    continue
 c
       if (amx.gt.amn) go to 3
-      write (lp,100) array(1,1)
+      write (*,100) array(1,1)
  100  format (//' field to be contoured is constant ...',1pe15.5/)
       return
 c
@@ -883,7 +894,7 @@ c
       if (ratio.le.sqrt2*5.)  contur=q*5.
       if (ratio.le.sqrt2*2.)  contur=q*2.
       if (ratio.le.sqrt2)     contur=q
-      write (lp,101) contur,amn,imn,jmn,amx,imx,jmx
+      write (*,101) contur,amn,imn,jmn,amx,imx,jmx
  101  format ('contour interval in plot below is',1pe9.1,
      . 6x,'min =',e11.3,'  at',2i5/48x,'max =',e11.3,'  at',2i5)
       call digplt(array,idim,ii,jj,contur)
@@ -896,7 +907,6 @@ c
 c
 c --- simulate a contour line plot on the printer
 c
-      USE HYCOM_SCALARS, only : lp
       implicit none
 c
       integer idim,ii,jj
@@ -929,10 +939,11 @@ c
      .     +array(ia+1,ja+1)*dxdy
       n=mod(mod(int(2.*value/dec+sign(.5,value)),20)+20,20)+1
  2    digit(j)=dig(n)
- 1    write (lp,100) 'i',' ',(digit(j),j=1,nchar+1),' ','i'
+ 1    write (*,100) 'i',' ',(digit(j),j=1,nchar+1),' ','i'
  100  format(1x,130a1)
       return
       end
+c
 c
       subroutine ParPsmoo(alist)
 c
@@ -969,5 +980,3 @@ c
  2    alist(i,j)=(1.-wgt-wgt)*blist(i,j)+wgt*(blist(ia,j)+blist(ib,j))
       return
       end
-c
-
