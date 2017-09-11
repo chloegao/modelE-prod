7876e5a147be2fde946fc8e6f42fff41933d930f
Greg Faluvegi <gregory.s.faluvegi@nasa.gov>
2017-09-11 10:45:54 -0400

    increasing time for lerner regression tests

--- exec/testing/regTools.py
+++ exec/testing/regTools.py
@@ -333,6 +333,8 @@ def createScriptTask(config, compconfig, deck, comp, mode):
                     walltime = '04:00:00'
                 elif re.search('vsd', deckName):
                     walltime = '01:00:00'
+                elif re.search('lerner', deckName):
+                    walltime = '01:00:00'
 
             # Adjust the walltime for some rundecks
             if re.search('C12', deckName):
@@ -350,6 +352,8 @@ def createScriptTask(config, compconfig, deck, comp, mode):
             elif re.search('Tmatrix', deckName):
                 walltime = '02:00:00'
             elif re.search('vsd', deckName):
+                walltime = '02:00:00'
+            elif re.search('lerner', deckName):
                 walltime = '02:00:00'
 
         outname = resultsDir + '/' + jobName + '.' + mode + '.out'
