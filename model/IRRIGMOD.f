e2dfae9d196b37845781b7ba47b3a40d8f7fa87b
Igor Aleinov <ialeinov@giss.nasa.gov>
2017-09-13 21:25:20 -0400

    Yet another fix to tracers in irrigation water. This one properly treats water isotopes.
    The default concentration of H2O18 and HDO in aquifers is set to
     -10 permil and -70 permil respectively. But one can set them to
    any number using rundeck parameters H2O18_in_aquifer and
    HDO_in_aquifer (in permil).

--- model/IRRIGMOD.f
+++ model/IRRIGMOD.f
@@ -27,6 +27,10 @@
 
       logical :: irrig_exists=.true.
 
+#ifdef TRACERS_WATER
+      real*8, dimension(:), allocatable :: tr_conc_in_aquifer
+#endif
+
       contains
 
       subroutine init_irrigmod
@@ -91,7 +95,39 @@
 
       call read_irrig(.false.)
 
+#ifdef TRACERS_WATER
+      call init_irrigmod_tracers
+#endif
+
       end subroutine init_irrigmod
+
+
+#ifdef TRACERS_WATER
+      subroutine init_irrigmod_tracers
+      USE TRACER_COM, only : ntm, n_H2O18, n_HDO, n_HTO, n_Water
+#ifdef TRACERS_SPECIAL_O18
+      real*8, external :: water_iso_conc_in_aquifer
+#endif
+      integer :: n
+
+      allocate(tr_conc_in_aquifer(ntm))
+
+      ! by defaut nothing is present in aquifer water except regular water
+      tr_conc_in_aquifer(:) = 0.0
+      if ( n_Water > 0 ) then
+        tr_conc_in_aquifer(n_Water) = 1.d0
+      endif
+
+#ifdef TRACERS_SPECIAL_O18
+      do n=1,ntm
+        if ( n==n_H2O18 .or. n==n_HDO .or. n==n_HTO ) then
+          tr_conc_in_aquifer(n) = water_iso_conc_in_aquifer(n)
+        endif
+      enddo
+#endif
+
+      end subroutine init_irrigmod_tracers
+#endif
 
 
       subroutine read_irrig(end_of_day)
@@ -201,7 +237,6 @@
 #ifdef TRACERS_WATER
       USE TRACER_COM, only : ntm
       USE FLUXES, only : atmlnd
-      use OldTracer_mod, only: nWATER, tr_wd_TYPE
 #endif
 ! fixed i,j arrays - feed in from call?
       USE GEOM, only : axyp
@@ -247,7 +282,6 @@ C**** Outputs
       real*8 :: m_to_kg
 !@param Flag for external irrigation source (from deep aquifers - not modeled) 
       integer :: flag_irrig_grndwat  ! =1 use ground water
-      integer :: n
 
 C**** set default output
       irrig_water_act = 0.d0 ; irrig_energy_act = 0.d0
@@ -324,16 +358,9 @@ C**** set default output
                irrig_gw        = irrig_water_act
                irrig_gw_energy = irrig_energy_act
 #ifdef TRACERS_WATER
-               do n=1,ntm
-                 if ( tr_wd_TYPE(n)==nWATER ) then
-                   irrig_tracer_act(n) =
-     &                  irrig_water_act*atmlnd%gtracer(n,i,j)
-                   irrig_gw_tracer(n) = irrig_gw*atmlnd%gtracer(n,i,j)
-                 else
-                   irrig_tracer_act(n) = 0.d0
-                   irrig_gw_tracer(n) = 0.d0
-                 endif
-               enddo
+               irrig_tracer_act(:) =
+     &              irrig_water_act*tr_conc_in_aquifer(:)
+               irrig_gw_tracer(:) = irrig_gw*tr_conc_in_aquifer(:)
 #endif
             else
                irrig_water_act = 0.d0
@@ -411,20 +438,10 @@ C**** set default output
                irrig_gw_energy  = irrig_energy_act - gml_to_irrig*rhow /
      *              (m_to_kg*dtsrc)  
 #ifdef TRACERS_WATER
-               do n=1,ntm
-                 if ( tr_wd_TYPE(n)==nWATER ) then
-                   irrig_tracer_act(n) =
-     &                  irrig_water_act*(trml_to_irrig(n,1)
-     *                  +trml_to_irrig(n,2)) / mwl_to_irrig
-                   irrig_gw_tracer(n) =
-     &                  irrig_tracer_act(n) - (trml_to_irrig(n,1)
-     *                  +trml_to_irrig(n,2)) / (m_to_kg*dtsrc)
-                 else
-                   irrig_tracer_act(n) =  (trml_to_irrig(n,1)
-     *                  +trml_to_irrig(n,2)) / (m_to_kg*dtsrc)
-                   irrig_gw_tracer(n) = 0.d0
-                 endif
-               enddo
+               irrig_gw_tracer(:) = irrig_gw*tr_conc_in_aquifer(:)
+               irrig_tracer_act(:) =  (trml_to_irrig(:,1)
+     *              + trml_to_irrig(:,2)) / (m_to_kg*dtsrc)
+     &              + irrig_gw_tracer(:)
 #endif
              else
                irrig_water_act  = m_avail / (m_to_kg*dtsrc) 
