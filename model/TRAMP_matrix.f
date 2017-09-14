ea5c08351cf495118633f200204178ef1b71a56b
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2017-09-14 16:00:46 -0400

    Fixing a typo to allow compilation for TRACERS_AMP_M9

--- model/TRAMP_matrix.f
+++ model/TRAMP_matrix.f
@@ -972,7 +972,7 @@
       PIQ(1,PROD_INDEX_SULF) = PIQ(1,PROD_INDEX_SULF) + DMDT_SO4  ! add secondary particle formation mass   term
 #ifdef TRACERS_AMP_M9
       DO i=1,NWEIGHTS
-IF (MSPCS(PROD_INDEX_OCM2,i) .eq. 0) CYCLE  !mspcs(nmass_spcs,nmodes_max),determine if vbs species is 0 or 1
+        IF (MSPCS(PROD_INDEX_OCM2,i) .eq. 0) CYCLE  !mspcs(nmass_spcs,nmodes_max),determine if vbs species is 0 or 1
         PIQ (i,PROD_INDEX_OCM2)=PIQ (i,PROD_INDEX_OCM2)+VBS_FLUXES(i,PROD_INDEX_OCM2)
         PIQ (i,PROD_INDEX_OCM1)=PIQ (i,PROD_INDEX_OCM1)+VBS_FLUXES(i,PROD_INDEX_OCM1)
         PIQ (i,PROD_INDEX_OCM0)=PIQ (i,PROD_INDEX_OCM0)+VBS_FLUXES(i,PROD_INDEX_OCM0)
