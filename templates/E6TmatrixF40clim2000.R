a59f9c60f76d92f40aad81e4117e1a6c476554f0
Greg Faluvegi <gregory.s.faluvegi@nasa.gov>
2017-09-18 12:22:11 -0400

    change E6 tracer _2000.R template names to
    clim2000.R, following today's push on master.
    This changes a TOMAS regression test template
    name too. Per Kostas's request, so they better
    match *clim.R template names.

--- templates/E6TmatrixF40clim2000.R
+++ templates/E6TmatrixF40clim2000.R
@@ -1,6 +1,7 @@
-E6TmatrixF40_2000.R GISS ModelE Lat-Lon Atmosphere Model, climatological ocn/atm MATRIX tracers
+E6TmatrixF40clim2000.R GISS ModelE Lat-Lon Atmosphere Model, climatological ocn/atm MATRIX tracers
 
-E6TmatrixF40_2000: E6TmatrixF40clim but year 2000 settings
+E6TmatrixF40clim2000: E6TmatrixF40clim but year 2000 settings
+               (e.g. 9-year averages centered around nominal date)
 
 Lat-lon: 2x2.5 degree horizontal resolution
 F40: 40 vertical layers with standard hybrid coordinate, top at .1 mb
@@ -108,7 +109,7 @@ MSU_wts=MSU_SSU_RSS_weights.txt      ! MSU-diag
 REG=REG2X2.5                      ! special regions-diag
 
 Label and Namelist:  (next 2 lines)
-E6TmatrixF40_2000 (climatological prescribed ocean atmospheric tracer model with MATRIX and Shindell chemistry)
+E6TmatrixF40clim2000 (climatological prescribed ocean atmospheric tracer model with MATRIX and Shindell chemistry)
 
 &&PARAMETERS
 #include "static_ocn_params"
