2405605bb693a86f8b59ba4fff912a8baf2e4130
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2017-10-07 08:52:43 -0400

    Increasing some MAX_XXXX parameters

--- model/shared/Dictionary_mod.F90
+++ model/shared/Dictionary_mod.F90
@@ -159,10 +159,10 @@ module Dictionary_mod
 
   end type Dictionary
 
-  integer, parameter :: MAX_PARAMS = 900
-  integer, parameter :: MAX_RPARAMS =700
+  integer, parameter :: MAX_PARAMS = 1000
+  integer, parameter :: MAX_RPARAMS = 700
   integer, parameter :: MAX_IPARAMS = 1200 
-  integer, parameter :: MAX_CPARAMS = 520
+  integer, parameter :: MAX_CPARAMS = 630
   integer, parameter :: MAX_NAME_LEN = 32
   integer, parameter :: MAX_CHAR_LEN = 128
 
