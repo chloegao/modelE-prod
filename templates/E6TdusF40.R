a4340bc637b1d5739de2033f927655c1ec2c9a10
jan perlwitz <jan.p.perlwitz@nasa.gov>
2017-11-14 12:20:11 -0500

    Fixed some descriptions in the CMIP6 interactive tracer rundecks for year 2000 climatological boundary conditions and for standalone interactive dust, which were saying something different to what the actual setup of the rundecks is.

--- templates/E6TdusF40.R
+++ templates/E6TdusF40.R
@@ -5,7 +5,7 @@ E6TdusF40: based on E6TomaF40. OMA tracers with computed dust only
 Lat-lon: 2x2.5 degree horizontal resolution
 F40: 40 vertical layers with standard hybrid coordinate, top at .1 mb
 Atmospheric composition for year 2000
-Ocean climatology prescribed from years 1876-1885, CMIP6
+Ocean climatology prescribed from years 1996-2005, CMIP6
 Uses turbulence scheme (no dry conv), grav.wave drag
 Time steps: dynamics 3.75 min leap frog; physics 30 min.; radiation 2.5 hrs
 Filters: U,V in E-W and N-S direction (after every physics time step)
