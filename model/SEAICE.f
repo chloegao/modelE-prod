738714d1ec08df7123376eb7c268a31e1e594f24
Gavin Schmidt <gavin.a.schmidt@nasa.gov>
2017-11-28 15:41:21 -0500

    minor fix to relayer_12 for a conservation of tracer problem.

--- model/SEAICE.f
+++ model/SEAICE.f
@@ -1,4 +1,3 @@
-
 #include "rundeck_opts.h"
 #ifdef TRACERS_ATM_ONLY
 #undef TRACERS_ON
@@ -894,7 +893,7 @@ C     SNOW*ROICEN = SNOW*roice  ! snow mass (kg) is conserved
       SNOWL(:) = SNOWL(:)*ROICE/ROICEN
       HSNOW(:) = HSNOW(:)*ROICE/ROICEN
       ROICE = ROICEN
-
+      
 C**** relayer upper two layers
         call relayer_12(HSNOW,HICE,SICE,MICE,SNOWL
 #ifdef TRACERS_WATER
@@ -1986,7 +1985,7 @@ C**** lower levels
 #ifdef TRACERS_WATER
       REAL*8, INTENT(INOUT) :: TRSNOW(NTM,2),TRICE(NTM,LMI)
       REAL*8 FTRSI1(NTM)
-#endif 
+#endif
       REAL*8 FMSI1,FHSI1,FSSI1,FMSI0
 
       FMSI1 = SNOWL(1)+MICE(1)-XSI(1)*(SNOWL(1)+SNOWL(2)+ACE1I)
@@ -2036,16 +2035,18 @@ C**** lower levels
           HSNOW(2)=HSNOW(2)+FHSI1
           HSNOW(1)=HSNOW(1)-FHSI1
 #ifdef TRACERS_WATER
-          TRSNOW(:,2)=TRSNOW(:,2)+(SNOWL(1)-FMSI0)*TRSNOW(:,1)/SNOWL(1)
-          TRSNOW(:,1)=TRSNOW(:,1)-(SNOWL(1)-FMSI0)*TRSNOW(:,1)/SNOWL(1)
+          FTRSI1(:) = (SNOWL(1)-FMSI0)*TRSNOW(:,1)/SNOWL(1)
+          TRSNOW(:,2)=TRSNOW(:,2)+FTRSI1(:)
+          TRSNOW(:,1)=TRSNOW(:,1)-FTRSI1(:)
 #endif
         ELSE
           FHSI1 = (FMSI0-SNOWL(1))*HSNOW(2)/SNOWL(2)
           HSNOW(2)=HSNOW(2)-FHSI1
           HSNOW(1)=HSNOW(1)+FHSI1
 #ifdef TRACERS_WATER
-          TRSNOW(:,2)=TRSNOW(:,2)-(FMSI0-SNOWL(1))*TRSNOW(:,2)/SNOWL(2)
-          TRSNOW(:,1)=TRSNOW(:,1)+(FMSI0-SNOWL(1))*TRSNOW(:,2)/SNOWL(2)
+          FTRSI1(:) = (FMSI0-SNOWL(1))*TRSNOW(:,2)/SNOWL(2)
+          TRSNOW(:,2)=TRSNOW(:,2)-FTRSI1(:)
+          TRSNOW(:,1)=TRSNOW(:,1)+FTRSI1(:)
 #endif
         ENDIF
         SNOWL(2)=SNOWL(1)+SNOWL(2)-FMSI0
@@ -2066,8 +2067,9 @@ C**** lower levels
         IF (MICE(1).gt.0) THEN ! flux ice and check for enough
           IF (FMSI1.gt.MICE(1)) THEN ! flux all ice and some snow  
 #ifdef TRACERS_WATER
-            TRSNOW(:,2)=TRSNOW(:,2)+(FMSI1-MICE(1))*TRSNOW(:,1)/SNOWL(1)
-            TRSNOW(:,1)=TRSNOW(:,1)-(FMSI1-MICE(1))*TRSNOW(:,1)/SNOWL(1)
+            FTRSI1(:) = (FMSI1-MICE(1))*TRSNOW(:,1)/SNOWL(1)
+            TRSNOW(:,2)=TRSNOW(:,2)+FTRSI1(:)
+            TRSNOW(:,1)=TRSNOW(:,1)-FTRSI1(:)
             TRICE(:,2) = TRICE(:,1)+TRICE(:,2)
             TRICE(:,1) = 0.
 #endif 
@@ -2157,7 +2159,7 @@ c           FSSI1 = 0.
           END IF
         END IF
       END IF
-
+      
       END IF
       
       return
