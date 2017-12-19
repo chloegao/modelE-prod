baa585271e0e3dafe60043f051e3cf302e2b51e9
Kostas Tsigaridis <kostas.tsigaridis@nasa.gov>
2017-12-19 17:28:07 -0500

    Restoring "_src" suffix in TRACER_XX input file sources, which was eliminated by 149ed6089d0e7ffa20b56cc4555d75b378aee8c1. If there are still _src suffixes missing under any configuration, please let me know.

--- model/tracers/TracerSurfaceSource.F90
+++ model/tracers/TracerSurfaceSource.F90
@@ -133,8 +133,9 @@ contains
         call stop_model('source name not found in file '//trim(fileName),255)
       endif
     endif
-    ! set default long name to be the same as the short name
-    this%sourceLname = this%sourceName
+    ! append ' source' to the long name, and '_src' to the short name
+    this%sourceLname = trim(this%sourceName)//' source'
+    this%sourceName = trim(this%sourceName)//'_src'
     
     ! -- begin sector stuff --
     tr_sectors_are = ' '
