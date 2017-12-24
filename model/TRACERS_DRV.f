chloegao/modelE-prod

commit 28737085745a10dc42e0806a786be71828cde1e5

tidying up of Lerner tracer diagnostics and surface source diagnostic duplicaitons. Some additional functionality for labeling Conservation diagnostics. No change to results.

model/TRDIAG_COM.f
@@ -597,14 +597,13 @@ C**** TCONSRV
 
 #ifdef TRACERS_ON
       SUBROUTINE SET_TCON(QCON,NAME_CON,QSUM,INST_UNIT,SUM_UNIT
-     *     ,INST_SC,CHNG_SC, itr,CONPTs)
+     *     ,INST_SC,CHNG_SC, itr,CONPTs,CONPT0)
 !@sum  SET_TCON assigns conservation diagnostic array indices
 !@auth Gavin Schmidt
       use TimeConstants_mod, only: SECONDS_PER_DAY
       USE MODEL_COM, only: dtsrc
       USE DYNAMICS, only : nfiltr
       USE DIAG_COM, only: npts,ia_d5d,ia_d5s,ia_filt,ia_12hr,ia_src
-     *     ,conpt0
       USE TRDIAG_COM, only: ktcon,title_tcon,scale_tcon,nsum_tcon
      *     ,nofmt,ia_tcon,name_tconsrv,lname_tconsrv,units_tconsrv
      *     ,ntcons,npts_common
@@ -633,6 +632,8 @@ C**** TCONSRV
       CHARACTER*16, INTENT(IN), DIMENSION(ntcons) :: CONPTS
 !@var CONPTS_sname like CONPTS but without spaces
       CHARACTER*16, DIMENSION(ntcons) :: CONPTS_sname
+!@var CONPT0 conservation diag points for common points
+      CHARACTER*10, INTENT(IN), DIMENSION(npts) :: CONPT0
 !@var CONPT0_sname like CONPT0 but without spaces
       CHARACTER*10, DIMENSION(npts) :: CONPT0_sname
       CHARACTER*11 CHGSTR

model/initTracerMetadata.f
@@ -136,40 +136,49 @@
 
 #ifdef TRACERS_SPECIAL_Lerner
         case ('N2O')
-          call addSurfaceSource(pTracer, "overwrite_at_surface")
+          call addSurfaceSource(pTracer, "overwrite_at_surface",
+     *          "Overwrite")
         case ('CFC11', 'Rn222')
-          call addSurfaceSource(pTracer, "surface_src")
+          call addSurfaceSource(pTracer, "surface_src", "Surface Src")
         case ('CH4')
-          call addSurfaceSource(pTracer, "animal_src")
-          call addSurfaceSource(pTracer, "coal_mine_src")
-          call addSurfaceSource(pTracer, "gas_leak_src")
-          call addSurfaceSource(pTracer, "gas_vent_src")
-          call addSurfaceSource(pTracer, "city_dump_src")
-          call addSurfaceSource(pTracer, "soil_sink")
-          call addSurfaceSource(pTracer, "termite_src")
-          call addSurfaceSource(pTracer, "coal_combustion_src")
-          call addSurfaceSource(pTracer, "ocean_src")
-          call addSurfaceSource(pTracer, "lake_src")
-          call addSurfaceSource(pTracer, "misc_ground_src")
-          call addSurfaceSource(pTracer, "biomass_src")
-          call addSurfaceSource(pTracer, "rice_src")
-          call addSurfaceSource(pTracer, "wetlands_tundra_src")
+          call addSurfaceSource(pTracer, "animal_src", "Animals")
+          call addSurfaceSource(pTracer, "coal_mine_src", "Coal Mines")
+          call addSurfaceSource(pTracer, "gas_leak_src", "Gas Leaks")
+          call addSurfaceSource(pTracer, "gas_vent_src", "Gas Venting")
+          call addSurfaceSource(pTracer, "city_dump_src", "City Dumps")
+          call addSurfaceSource(pTracer, "soil_sink", "Soil Sink")
+          call addSurfaceSource(pTracer, "termite_src", "Termites")
+          call addSurfaceSource(pTracer, "coal_combustion_src", 
+     *         "Coal Combustion")
+          call addSurfaceSource(pTracer, "ocean_src", "Ocean Src")
+          call addSurfaceSource(pTracer, "lake_src", "Lake Src")
+          call addSurfaceSource(pTracer, "misc_ground_src", "Misc Src")
+          call addSurfaceSource(pTracer, "biomass_src", "Biomass")
+          call addSurfaceSource(pTracer, "rice_src", "Rice")
+          call addSurfaceSource(pTracer, "wetlands_tundra_src",
+     *         "WetlandsTundra")
         case ('O3')
-          call addSurfaceSource(pTracer, "deposition_sink")
+          call addSurfaceSource(pTracer, "deposition_sink",
+     *          "Deposition")
         case ('CO2')
-          call addSurfaceSource(pTracer, "fossil_fuel_src")
-          call addSurfaceSource(pTracer, "fertilization_sink")
-          call addSurfaceSource(pTracer, "north_forest_regrowth_src")
-          call addSurfaceSource(pTracer, "land_use_modification")
-          call addSurfaceSource(pTracer, "ecosystem_exchange")
-          call addSurfaceSource(pTracer, "ocean_exchange")
+          call addSurfaceSource(pTracer, "fossil_fuel_src",
+     *          "Fossil Fuel")
+          call addSurfaceSource(pTracer, "fertilization_sink",
+     *         "Fertilization")
+          call addSurfaceSource(pTracer, "north_forest_regrowth_src",
+     *         "Forest Regrow")
+          call addSurfaceSource(pTracer, "land_use_modification",
+     *         "Land Use")
+          call addSurfaceSource(pTracer, "ecosystem_exchange",
+     *         "Ecosystem Exch")
+          call addSurfaceSource(pTracer, "ocean_exchange", "Ocean Exch")
         case ('14CO2')
-          call addSurfaceSource(pTracer, "surface_sink")
+          call addSurfaceSource(pTracer, "surface_sink", "Surface Sink")
 #endif  /* TRACERS_SPECIAL_Lerner */
 
 #ifdef TRACERS_SF6
         case ('SF6', 'SF6_c')
-          call addSurfaceSource(pTracer, "surface_src")
+          call addSurfaceSource(pTracer, "surface_src", "Surface Src")
 #endif  /* TRACERS_SF6 */
 
         end select

model/tracers/Tracer.F90
@@ -241,12 +241,18 @@ contains
   ! Use this routine to add a new surface source that
   ! is manipulated by custom logic elsewhere.
   ! Optional sourcename is only used by diagnostics
-  subroutine addSurfaceSource(this, sourceName)
+  subroutine addSurfaceSource(this, sourceName, sourceLname)
     type (Tracer), intent(inout) :: this
     character(len=*), intent(in) :: sourceName
-
+    character(len=*), intent(in), optional :: sourceLname
+    
     this%ntSurfSrc = this%ntSurfSrc + 1
     this%surfaceSources(this%ntSurfSrc)%sourceName = sourceName
+    if (present(sourceLname) ) then
+       this%surfaceSources(this%ntSurfSrc)%sourceLname = sourceLname
+    else
+       this%surfaceSources(this%ntSurfSrc)%sourceLname = sourceName
+    end if
 
   end subroutine addSurfaceSource
 

model/tracers/TracerSurfaceSource.F90
@@ -20,7 +20,8 @@ module TracerSurfaceSource_mod
 
   type, extends(TracerSource) :: TracerSurfaceSource
     character(len=30) :: sourceName ! holds source name, read from file header, e.g. to be
-    ! placed into lname and sname arrays.
+    ! placed into sname arrays.
+    character(len=30) :: sourceLname ! holds long source name
 !@var EMstream interface for reading and time-interpolating emissions file if it is netcdf.
 !@+   See usage notes in timestream_mod.
     type (timestream) :: EMstream
@@ -132,6 +133,9 @@ contains
       endif
     endif
 
+    ! set default long name to be the same as the short name
+    this%sourceLname = this%sourceName
+	    
     ! -- begin sector stuff --
     tr_sectors_are = ' '
     pname=trim(trim(fileName)//'_sect')

commit 87499ac18fcefd149c1092fa3f028a528677d54c

Eliminating ntsurfsrc explicit definition and corresponding explicit coding for ANUM__XX, which also resolves the bug introduced by 28737085745a10dc42e0806a786be71828cde1e5.

model/TRACERS_DRV.f
@@ -848,12 +848,6 @@ C**** set defaults for some precip/wet-dep related diags
 
 ! surface emissions
       do kk=1,ntsurfsrc(n_src)
-        select case (trname(n))
-        case ('ANUM__01','ANUM__02','ANUM__03','ANUM__04','ANUM__05',
-     &        'ANUM__06','ANUM__07','ANUM__08','ANUM__09','ANUM__10',
-     &        'ANUM__11','ANUM__12','ANUM__13','ANUM__14','ANUM__15')
-          cycle ! these tracers contain hardcoded ntsurfsrc, skip them
-        end select
         k = k + 1
         jls_source(kk,n) = k
         sname_jls(k) = trim(trname(n))//'_'//
@@ -1659,9 +1653,6 @@ c SO4
 c industrial source
         do kk=1,ntsurfsrc(n_ANUM(1))
           k = k + 1
-         IF(kk.eq.1) sources(kk)%sourceName='SO4'
-         IF(kk.eq.2) sources(kk)%sourceName='EC'
-         IF(kk.eq.3) sources(kk)%sourceName='OC'
           jls_source(kk,n) = k
           sname_jls(k) = trim(trname(n))//'_'//
      &                   trim(sources(kk)%sourceName)//'_src'
@@ -2410,12 +2401,6 @@ C**** This needs to be 'hand coded' depending on circumstances
 
 ! surface emissions
       do kr=1,ntsurfsrc(n_src)
-        select case (trname(n))
-        case ('ANUM__01','ANUM__02','ANUM__03','ANUM__04','ANUM__05',
-     &        'ANUM__06','ANUM__07','ANUM__08','ANUM__09','ANUM__10',
-     &        'ANUM__11','ANUM__12','ANUM__13','ANUM__14','ANUM__15')
-          cycle ! these tracers contain hardcoded ntsurfsrc, skip them
-        end select
         k = k+1
         ijts_source(kr,n) = k
         ia_ijts(k) = ia_src
@@ -3377,10 +3362,6 @@ c put in production of SO4 from gas phase
 c SO4 from industrial emissions
         do kr=1,ntsurfsrc(n_ANUM(1))
           k = k + 1
-         IF(kr.eq.1) sources(kr)%sourceName='SO4'
-         IF(kr.eq.2) sources(kr)%sourceName='EC'
-         IF(kr.eq.3) sources(kr)%sourceName='OC'
-
           ijts_source(kr,n) = k
           ia_ijts(k) = ia_src
           sname_ijts(k) = trim(trname(n))//'_'//

model/TRDIAG_COM.f
@@ -359,7 +359,7 @@ C**** TAJLS  <<<< KTAJLS and JLS_xx are Tracer-Dependent >>>>
 #ifndef TRACERS_TOMAS
       INTEGER,PARAMETER :: ktajls=1350
 #else
-      INTEGER,PARAMETER :: ktajls=3285 
+      INTEGER,PARAMETER :: ktajls=3400
 #endif
 !@var jls_XXX index for non-tracer specific or special diags
       INTEGER jls_OHconk,jls_HO2con,jls_NO3,jls_O3vmr
@@ -421,7 +421,7 @@ C**** TCONSRV
      &                             +4
 #endif
 #ifdef TRACERS_TOMAS
-     &                             +6
+     &                             +6+15
 #endif
 !@param npts_common total number of conservation diagnostics outside
 !@+                 those defined for tracers

model/TomasTracersMetadata.F90
@@ -143,7 +143,6 @@ contains
       if(bin.le.5) call set_ntm_power(n, 10)
       if(bin.gt.5) call set_ntm_power(n, 8) 
 
-      call set_ntsurfsrc(n,  3) ! 1 for SO4,2 for EC, 3 for OC (4 for SS and 5 for DU)
       call set_tr_mm(n, 1.d+0)
       call set_trpdens(n, TOMAS_dens)
       call set_trradius(n, TOMAS_radius)

model/initTracerMetadata.f
@@ -125,7 +125,8 @@
 #if (defined TRACERS_AEROSOLS_Koch) || (defined TRACERS_AMP) ||\
         (defined TRACERS_TOMAS)
         case ('OCII', 'M_OCC_OC', 'SOAgas') ! this handles OCT_src (terpene source)
-          call addSurfaceSource(pTracer, "Terpene_source")
+          call addSurfaceSource(pTracer, "Terpenes_src",
+     &                                   "Terpenes source")
 #endif
 #endif  /* TRACERS_AEROSOLS_SOA */
 
@@ -180,6 +181,15 @@
         case ('SF6', 'SF6_c')
           call addSurfaceSource(pTracer, "surface_src", "Surface Src")
 #endif  /* TRACERS_SF6 */
+
+#ifdef TRACERS_TOMAS
+        case ('ANUM__01','ANUM__02','ANUM__03','ANUM__04','ANUM__05',
+     &        'ANUM__06','ANUM__07','ANUM__08','ANUM__09','ANUM__10',
+     &        'ANUM__11','ANUM__12','ANUM__13','ANUM__14','ANUM__15')
+          call addSurfaceSource(pTracer, "SO4_src", "SO4 source")
+          call addSurfaceSource(pTracer, "EC_src", "EC source")
+          call addSurfaceSource(pTracer, "OC_src", "OC source")
+#endif  /* TRACERS_TOMAS */
 
         end select
 