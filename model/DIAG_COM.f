4c517695babfab6f83ad2f7a08622910bcd5deac
Andrew Ackerman <andrew.ackerman@nasa.gov>
2019-06-24 15:13:33 -0400

    Added monthly mean AIJ diagnostics for stratiform cloud droplet size and number concentration and activated CCN number concentration, all near stratiform liquid-phase cloud top.

--- model/DIAG_COM.f
+++ model/DIAG_COM.f
@@ -567,6 +567,9 @@ C****      names, indices, units, idacc-numbers, etc.
      *     IJ_GICE, IJ_GWTR1, IJ_ZSNOW, IJ_AFLMLT, IJ_AERUNS, IJ_AERUNU,
      *     IJ_HTSOIL, IJ_HTSNOW, IJ_AINTRCP, IJ_MCCLDTP, IJ_MCCLDBS,
      *     IJ_TRSDN,
+#ifdef CLD_AER_CDNC
+     *     ij_tclssct,ij_rclssct,ij_nclssct,ij_ccnssct,
+#endif
      *     IJ_TRSUP, IJ_CLDW, IJ_CLDI, IJ_QM, IJ_SSH, IJ_FWOC,
      *     IJ_DSKIN, IJ_DSKINSNOW, IJ_MCCVTP, IJ_MCCVBS,
      *     IJ_LI, IJ_LK,
